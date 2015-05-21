8f905a0dfa15aa4e4279b44c5a23e6ddf873b534
Kyle Napod <knnapod@puc.edu>
2015-05-21 15:52:45 -0700

    Finished Working Copy
    AI goes wherever it sees an empty space. Final commit. AI works.

--- lib/tictactoe/ai.rb
+++ lib/tictactoe/ai.rb
@@ -8,23 +8,11 @@ module TicTacToe
       @sign = sign
     end
     
-    # Prompts the user for a valid move
-    #
-    # @param responder [Proc] where the prompt should print to.
-    # @return [Integer] the index entered by the user
-    def prompt(&responder)
-      responder ||= lambda { gets }
-      responder.call.chomp.to_i
-    end
-    
-    # Display the board and loop the prompt untill the user enters a valid index
-    #
-    # @param board [Board] the board to get a move for
-    # @return [Integer] the index to play at
+    # Makes AI go in first possible empty space.
     def get_move(board)
-      puts board.to_s
-      until board.valid_move?(move = prompt)
-        print "#{move} is not valid. "
+      move = 0
+      until board.valid_move?(move)
+        move += 1
       end
       return move
     end
